cc43c3bc1061c5bb752533b5884b5cdf9aa759a7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-09 19:44:14 +0300

    Initial Commit

---
+++ HowToCreateSmoothTransitionsInSwiftUI/HowToCreateSmoothTransitionsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateSmoothTransitionsInSwiftUIApp.swift
+//  HowToCreateSmoothTransitionsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 09.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateSmoothTransitionsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
